af3ba28f0ff8d451fab6640b17b968b65a2f19e7
xabertum <mohatar@gmail.com>
2017-10-11 18:55:27 +0200

    Minor changes

--- src/java_test/Java_Test.java
+++ src/java_test/Java_Test.java
@@ -39,59 +39,18 @@ public class Java_Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola a todos");
-        System.out.println(new Date());
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca un numero");
-        sc.nextInt();
+        int[][] b = new int[2][2];
+        int count = 0;
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[i].length; j++) {
+                b[i][j] = count;
+                count++;
+            }
 
-        Java_Test jt = new Java_Test(3, 5);
-
-        System.out.println(cuantosObjetos());
-
-        System.out.println("Fin del programa");
-
-        System.out.println(jt.x + jt.y);
-        
-        Ejecutivo jefe = new Ejecutivo();
-        Empleado empleado = new Empleado("j",36);
-        
-        System.err.println("Numero de empleado: " + jefe.numEmpleado);
-        System.err.println("Numero de empleado: " + empleado.numEmpleado);
-        
-        System.out.println(empleado.nombre);
-        jefe.asignarPresupuesto(2000);
-        System.err.println("Categoria Ejecutiva: " + jefe.categoriaEjecutiva);
-        
-        System.out.println("Introduzca la palabra: ");
-        String testString = sc.next();
-        
-        String testString1 = "Hola";
-        
-        if (testString.equals(testString1)) {
-            System.err.println("Acierto!");
-        
-        } else {
-            System.err.println("ERROR!");
         }
-        
-        //_-------------------------------_//
-        Coche vehiculo1 = new Coche();
-        vehiculo1.acelerar(50);
-        vehiculo1.frenar(34);
-        System.err.println(vehiculo1.obtenerVelocidad());
-        
-        
-        //--------------------------------------//
-        
-        Matematicas m1 = new Matematicas();
-        double x = m1.factorial(5);
-        System.out.println(x);
-        
-        
-        
 
+        System.out.println(count);
     }
 
 }
